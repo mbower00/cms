1e61ba396611b36eac3d3f93344fee77c1d71ab5
Mitchell Bower <mitchbbower@gmail.com>
2025-05-02 19:43:43 -0400

    contact list

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
 export class ContactListComponent {
-
+  // dummy data below from online instructions
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null
+    ),
+  ];
 }
