42e6e8d42433801170e0c73c2f5ebf7e7bcf6da0
Mitchell Bower <mitchbbower@gmail.com>
2025-05-08 15:09:02 -0400

    documents

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrl: './document-list.component.css',
 })
 export class DocumentListComponent {
-
+  documents: Document[];
 }
